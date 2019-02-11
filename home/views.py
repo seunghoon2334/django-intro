fdd85188def1da719e5c2fc327d32c9d879001e6
seunghoon2334 <gogo12394@naver.com>
2019-02-11 02:32:43 +0000

    03_template render

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
@@ -5,4 +6,16 @@ def index(request): # @app.route('') 아래 def index의 기능을 views.py가 �
     print(request)
     print(type(request))
     print(request.META)
-    return HttpResponse('hello, django!')
+    return render(request, 'index.html')
+    
+def dinner(request):
+    box = ['처갓집치킨', 'BHC치킨', '햄버거', '식빵', '토스트', '고구마맛탕', '피자', '핫도그', '충만치킨',' 삼합', 'emoi', '푸라닭치킨']
+    dinner = random.choice(box)
+    # return HttpResponse(dinner)
+    # render 필수인자
+    # 1) request, 2) template 파일(html)
+    # render 선택인자
+    # 3) dictionary : 템플릿에서 쓸 변수 값을 정의
+    return render(request, 'dinner.html', {'dinner': dinner})
+    # template은 기본적으로 문법이 jinja2랑 비슷한데, 장고에서는 DTL을 쓴다.
+    # Django Template Language
